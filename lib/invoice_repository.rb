f134d51577c1d46c5b252e0ca559a840d17e06e7
mrjaimisra <misrajai01@gmail.com>
2015-06-20 13:37:20 -0600

    changed method names in invoice repo to match spec harness

--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -22,51 +22,51 @@ class InvoiceRepository
     invoices.shuffle.first
   end
 
-  def find_invoice_by_id(id)
+  def find_by_id(id)
     invoices.find { |invoice| invoice.id == id }
   end
 
-  def find_invoice_by_customer_id(customer_id)
+  def find_by_customer_id(customer_id)
     invoices.find { |invoice| invoice.customer_id == customer_id }
   end
 
-  def find_invoice_by_merchant_id(merchant_id)
+  def find_by_merchant_id(merchant_id)
     invoices.find { |invoice| invoice.merchant_id == merchant_id }
   end
 
-  def find_invoice_by_status(status)
+  def find_by_status(status)
     invoices.find { |invoice| invoice.status == status }
   end
 
-  def find_invoice_by_created_at(created_at)
+  def find_by_created_at(created_at)
     invoices.find { |invoice| invoice.created_at == created_at }
   end
 
-  def find_invoice_by_updated_at(updated_at)
+  def find_by_updated_at(updated_at)
     invoices.find { |invoice| invoice.updated_at == updated_at }
   end
 
-  def find_all_invoices_by_id(id)
+  def find_all_by_id(id)
     invoices.select { |invoice| invoice.id == id }
   end
 
-  def find_all_invoices_by_customer_id(customer_id)
+  def find_all_by_customer_id(customer_id)
     invoices.select { |invoice| invoice.customer_id == customer_id }
   end
 
-  def find_all_invoices_by_merchant_id(merchant_id)
+  def find_all_by_merchant_id(merchant_id)
     invoices.select { |invoice| invoice.merchant_id == merchant_id }
   end
 
-  def find_all_invoices_by_status(status)
+  def find_all_by_status(status)
     invoices.select { |invoice| invoice.status == status }
   end
 
-  def find_all_invoices_by_created_at(created_at)
+  def find_all_by_created_at(created_at)
     invoices.select { |invoice| invoice.created_at == created_at }
   end
 
-  def find_all_invoices_by_updated_at(updated_at)
+  def find_all_by_updated_at(updated_at)
     invoices.select { |invoice| invoice.updated_at == updated_at }
   end
 
