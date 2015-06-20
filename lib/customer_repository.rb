f6da6eb8d158fe4b22791a6601eafa03000293e5
mrjaimisra <misrajai01@gmail.com>
2015-06-20 13:27:02 -0600

    fixed customer repo variable names to match spec harness'

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -26,39 +26,39 @@ class CustomerRepository
     customers.find { |customer| customer.id == id }
   end
 
-  def find_customer_by_first_name(first_name)
+  def find_by_first_name(first_name)
     customers.find { |customer| customer.first_name == first_name }
   end
 
-  def find_customer_by_last_name(last_name)
+  def find_by_last_name(last_name)
     customers.find { |customer| customer.last_name == last_name }
   end
 
-  def find_customer_by_created_at(created_at)
+  def find_by_created_at(created_at)
     customers.find { |customer| customer.created_at == created_at }
   end
 
-  def find_customer_by_updated_at(updated_at)
+  def find_by_updated_at(updated_at)
     customers.find { |customer| customer.updated_at == updated_at }
   end
 
-  def find_all_customers_by_id(id)
+  def find_all_by_id(id)
     customers.select { |customer| customer.id == id }
   end
 
-  def find_all_customers_by_first_name(first_name)
+  def find_all_by_first_name(first_name)
     customers.select { |customer| customer.first_name == first_name }
   end
 
-  def find_all_customers_by_last_name(last_name)
+  def find_all_by_last_name(last_name)
     customers.select { |customer| customer.last_name == last_name }
   end
 
-  def find_all_customers_by_created_at(created_at)
+  def find_all_by_created_at(created_at)
     customers.select { |customer| customer.created_at == created_at }
   end
 
-  def find_all_customers_by_updated_at(updated_at)
+  def find_all_by_updated_at(updated_at)
     customers.select { |customer| customer.updated_at == updated_at }
   end
 
