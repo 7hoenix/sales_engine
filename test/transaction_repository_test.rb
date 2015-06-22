076a0d5d8061d44e727f6eeaf1c9e1a3f7549753
philmphoenix <jpholzmann@gmail.com>
2015-06-22 10:46:16 -0600

    transaction repo fixed

--- test/transaction_repository_test.rb
+++ test/transaction_repository_test.rb
@@ -94,21 +94,21 @@ class TransactionRepositoryTest < Minitest::Test
   def test_it_finds_the_first_transaction_by_created_at
     sales_engine = "MY ENGINE"
 
-    time3 = Time.new - 3
-    time5 = Time.new - 5
-    time7 = Time.new - 7
+    date1 = "Tue, 20 Mar 2012"
+    date2 = "Tue, 19 Mar 2012"
+    date3 = "Tue, 18 Mar 2012"
 
     hashes = [
-        {id: 1, created_at: (time3)},
-        {id: 2, created_at: (time5)},
-        {id: 3, created_at: (time7)},]
+        {id: 1, created_at: date1},
+        {id: 2, created_at: date2},
+        {id: 3, created_at: date3},]
     repo = TransactionRepository.new(hashes, sales_engine)
 
-    first_created_at = repo.find_by_created_at(time5)
+    first_created_at = repo.find_by_created_at(Date.parse(date2))
 
     assert_equal [2], [first_created_at.id]
 
-    second_created_at = repo.find_by_created_at(time7)
+    second_created_at = repo.find_by_created_at(Date.parse(date3))
     assert_equal [3], [second_created_at.id]
 
     # tv = repo.find_item_by_name("TV")
@@ -119,21 +119,21 @@ class TransactionRepositoryTest < Minitest::Test
   def test_it_finds_the_first_transaction_by_updated_at
     sales_engine = "MY ENGINE"
 
-    time3 = Time.new - 3
-    time5 = Time.new - 5
-    time7 = Time.new - 7
+    date1 = "Tue, 20 Mar 2012"
+    date2 = "Tue, 19 Mar 2012"
+    date3 = "Tue, 18 Mar 2012"
 
     hashes = [
-        {id: 1, updated_at: time3},
-        {id: 2, updated_at: time5},
-        {id: 3, updated_at: time7},]
+        {id: 1, updated_at: date1},
+        {id: 2, updated_at: date2},
+        {id: 3, updated_at: date3},]
     repo = TransactionRepository.new(hashes, sales_engine)
 
-    first_updated_at = repo.find_by_updated_at(time3)
+    first_updated_at = repo.find_by_updated_at(Date.parse(date1))
 
     assert_equal [1], [first_updated_at.id]
 
-    second_updated_at = repo.find_by_updated_at(time5)
+    second_updated_at = repo.find_by_updated_at(Date.parse(date2))
     assert_equal [2], [second_updated_at.id]
 
     # tv = repo.find_item_by_name("TV")
@@ -227,24 +227,25 @@ class TransactionRepositoryTest < Minitest::Test
 
   def test_it_finds_all_transactions_by_created_at
 
-    time3 = Time.new - 3
-    time5 = Time.new - 5
-    time7 = Time.new - 7
+
+    date1 = "Tue, 20 Mar 2012"
+    date2 = "Tue, 19 Mar 2012"
+    date3 = "Tue, 18 Mar 2012"
 
     sales_engine = "MY ENGINE"
     hashes = [
-        {id: 1, created_at: (time3)},
-        {id: 2, created_at: (time5)},
-        {id: 3, created_at: (time7)},
-        {id: 5, created_at: (time7)},]
+        {id: 1, created_at: date1},
+        {id: 2, created_at: date2},
+        {id: 3, created_at: date3},
+        {id: 5, created_at: date3},]
 
     repo = TransactionRepository.new(hashes, sales_engine)
 
-    first_all_created_at = repo.find_all_by_created_at(time7)
+    first_all_created_at = repo.find_all_by_created_at(Date.parse(date3))
 
     assert_equal [3, 5], first_all_created_at.map { |item| item.id}
 
-    second_created_at = repo.find_all_by_created_at(time5)
+    second_created_at = repo.find_all_by_created_at(Date.parse(date2))
     assert_equal [2], second_created_at.map { |item| item.id}
 
     # tv = repo.find_item_by_name("TV")
@@ -254,22 +255,22 @@ class TransactionRepositoryTest < Minitest::Test
 
   def test_it_finds_all_transactions_by_updated_at
 
-    time3 = Time.new - 3
-    time5 = Time.new - 5
-    time7 = Time.new - 7
+    date1 = "Tue, 20 Mar 2012"
+    date2 = "Tue, 19 Mar 2012"
+    date3 = "Tue, 18 Mar 2012"
 
     sales_engine = "MY ENGINE"
     hashes = [
-        {id: 1, updated_at: time3},
-        {id: 2, updated_at: time5},
-        {id: 3, updated_at: time7},]
+        {id: 1, updated_at: date1},
+        {id: 2, updated_at: date2},
+        {id: 3, updated_at: date3},]
     repo = TransactionRepository.new(hashes, sales_engine)
 
-    first_updated_at = repo.find_by_updated_at(time3)
+    first_updated_at = repo.find_by_updated_at(Date.parse(date1))
 
     assert_equal [1], [first_updated_at.id]
 
-    second_updated_at = repo.find_by_updated_at(time5)
+    second_updated_at = repo.find_by_updated_at(Date.parse(date2))
     assert_equal [2], [second_updated_at.id]
 
     # tv = repo.find_item_by_name("TV")
